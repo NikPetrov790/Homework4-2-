0e2ea08a4e6e97f2f991a7e08189cbd7b84e1add
Petrov Nikolay <nikolay.petrov99@mail.ru>
2022-09-03 12:18:40 +0300

    Check

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+string inputNumber = ReadLine();
+int[] numbers = ConvertToIntArray(inputNumber);
+WriteLine($"{inputNumber} -> {Sum(numbers)}");
+
+int[] ConvertToIntArray(string str) {
+    int[] numbers = new int[str.Length];
+    for(int i = 0; i < str.Length; i++) {
+        numbers[i] = (int) Char.GetNumericValue(str[i]);
+    }
+    return numbers;
+}
+
+int Sum(int[] numbers) {
+    int sum = 0;
+    for(int i = 0; i < numbers.Length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
